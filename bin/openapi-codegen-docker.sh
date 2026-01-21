97a5ca9574de265c2887a76858de41eea0b80d8a
necipallef <necipallef@gmail.com>
2026-01-21 22:00:32 +0300

    chore: fix readme generation

--- bin/openapi-codegen-docker.sh
+++ bin/openapi-codegen-docker.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+mv README.md temp.README.md
+
 docker run --rm -v "${PWD}:/local" openapitools/openapi-generator-cli generate \
     -i /local/res/fingerprint-server-api-v4.yaml \
     -g go \
     -o /local \
     -t /local/template \
     -p packageName=sdk
+
+mv README.md README.generated.md
+mv temp.README.md README.md
 
 gofmt -w .
