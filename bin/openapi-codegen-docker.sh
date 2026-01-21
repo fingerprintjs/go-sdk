169c6daa19dd7d75e00655bf46208555fb29b606
necipallef <necipallef@gmail.com>
2026-01-21 21:45:12 +0300

    chore: fix code generation script

--- bin/openapi-codegen-docker.sh
+++ bin/openapi-codegen-docker.sh
@@ -3,5 +3,8 @@
 docker run --rm -v "${PWD}:/local" openapitools/openapi-generator-cli generate \
     -i /local/res/fingerprint-server-api-v4.yaml \
     -g go \
-    -o /local/out/go \
-    -t /local/template
+    -o /local \
+    -t /local/template \
+    -p packageName=sdk
+
+gofmt -w .
