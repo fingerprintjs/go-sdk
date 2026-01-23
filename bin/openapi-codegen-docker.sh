6dd7377830fef0b418b820f55f905acfafff7f52
necipallef <necipallef@gmail.com>
2026-01-23 11:19:07 +0300

    chore: refactor  generator script

--- bin/openapi-codegen-docker.sh
+++ bin/openapi-codegen-docker.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-mv README.md temp.README.md
+set -e
 
-docker run --rm -v "${PWD}:/local" openapitools/openapi-generator-cli generate \
-    -i /local/res/fingerprint-server-api-v4.yaml \
-    -g go \
-    -o /local \
-    -t /local/template \
-    -p packageName=fingerprint
+run_generator() {
+  docker run --rm -v "${PWD}:/local" openapitools/openapi-generator-cli generate \
+      -i /local/res/fingerprint-server-api-v4.yaml \
+      -g go \
+      -o /local \
+      -t /local/template \
+      -p packageName=fingerprint,gitUserId=fingerprintjs,gitRepoId=fingerprint-server-go-sdk
+}
 
-mv README.md README.generated.md
-mv temp.README.md README.md
+mv README.md temp.README.md # hide manual README
+trap 'mv -f temp.README.md README.md' EXIT # restore manual README
+run_generator
+mv README.md README.generated.md # move generated README
 
 gofmt -w .
