9311f151849219f73223c3e69c0f56148716f743
Dan McNulty <daniel.mcnulty@fingerprint.com>
2026-01-29 15:57:19 -0600

    test: update UpdateEvent_test for v4

--- test/UpdateEvent_test.go
+++ test/UpdateEvent_test.go
@@ -2,7 +2,9 @@ package test
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -11,8 +13,10 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var trueValue = true
-var linkedIdValue = "linked_id"
+var (
+	trueValue     = true
+	linkedIdValue = "linked_id"
+)
 
 func TestUpdateEvent(t *testing.T) {
 	t.Run("Returns 200 on success", func(t *testing.T) {
@@ -20,13 +24,17 @@ func TestUpdateEvent(t *testing.T) {
 			w http.ResponseWriter,
 			r *http.Request,
 		) {
+			body, err := io.ReadAll(r.Body)
+			assert.NoError(t, err)
+
+			assert.Equal(t, `{"linked_id":"linked_id","suspect":true}` + "\n", string(body))
+
 			integrationInfo := r.URL.Query().Get("ii")
 			assert.Equal(t, integrationInfo, fmt.Sprintf("fingerprint-pro-server-go-sdk/%s", fingerprint.Version))
 			assert.Equal(t, r.URL.Path, "/events/123")
 			assert.Equal(t, r.Method, http.MethodPatch)
 
-			apiKey := r.Header.Get("Authorization")
-			assert.Equal(t, apiKey, "Bearer api_key")
+			assertAuthorizationHeader(t, r, "api_key")
 
 			w.Header().Set("Content-Type", "application/json")
 			w.WriteHeader(200)
@@ -46,379 +54,182 @@ func TestUpdateEvent(t *testing.T) {
 		assert.Equal(t, res.StatusCode, 200)
 	})
 
-	//t.Run("Update with empty body", func(t *testing.T) {
-	//	ts := httptest.NewServer(http.HandlerFunc(func(
-	//		w http.ResponseWriter,
-	//		r *http.Request,
-	//	) {
-	//		body, err := io.ReadAll(r.Body)
-	//		assert.NoError(t, err)
-	//
-	//		assert.Equal(t, "{}", string(body))
-	//
-	//		configFile := config.ReadConfig("../config.json")
-	//		integrationInfo := r.URL.Query().Get("ii")
-	//		assert.Equal(t, integrationInfo, fmt.Sprintf("fingerprint-pro-server-go-sdk/%s", configFile.PackageVersion))
-	//		assert.Equal(t, r.URL.Path, "/events/123")
-	//		assert.Equal(t, r.Method, http.MethodPut)
-	//
-	//		apiKey := r.Header.Get("Auth-Api-Key")
-	//		assert.Equal(t, apiKey, "api_key")
-	//
-	//		w.Header().Set("Content-Type", "application/json")
-	//		w.WriteHeader(200)
-	//	}))
-	//	defer ts.Close()
-	//
-	//	cfg := sdk.NewConfiguration()
-	//	cfg.ChangeBasePath(ts.URL)
-	//
-	//	client := sdk.NewAPIClient(cfg)
-	//
-	//	ctx := context.WithValue(context.Background(), sdk.ContextAPIKey, sdk.APIKey{
-	//		Key: "api_key",
-	//	})
-	//
-	//	res, err := client.FingerprintApi.UpdateEvent(ctx, sdk.EventsUpdateRequest{}, "123")
-	//
-	//	assert.Nil(t, err)
-	//	assert.NotNil(t, res)
-	//	assert.Equal(t, res.StatusCode, 200)
-	//})
-	//
-	//t.Run("Update with empty tag", func(t *testing.T) {
-	//	ts := httptest.NewServer(http.HandlerFunc(func(
-	//		w http.ResponseWriter,
-	//		r *http.Request,
-	//	) {
-	//		body, err := io.ReadAll(r.Body)
-	//		assert.NoError(t, err)
-	//
-	//		assert.Equal(t, "{}", string(body))
-	//
-	//		configFile := config.ReadConfig("../config.json")
-	//		integrationInfo := r.URL.Query().Get("ii")
-	//		assert.Equal(t, integrationInfo, fmt.Sprintf("fingerprint-pro-server-go-sdk/%s", configFile.PackageVersion))
-	//		assert.Equal(t, r.URL.Path, "/events/123")
-	//		assert.Equal(t, r.Method, http.MethodPut)
-	//
-	//		apiKey := r.Header.Get("Auth-Api-Key")
-	//		assert.Equal(t, apiKey, "api_key")
-	//
-	//		w.Header().Set("Content-Type", "application/json")
-	//		w.WriteHeader(200)
-	//	}))
-	//	defer ts.Close()
-	//
-	//	cfg := sdk.NewConfiguration()
-	//	cfg.ChangeBasePath(ts.URL)
-	//
-	//	client := sdk.NewAPIClient(cfg)
-	//
-	//	ctx := context.WithValue(context.Background(), sdk.ContextAPIKey, sdk.APIKey{
-	//		Key: "api_key",
-	//	})
-	//
-	//	var tag sdk.ModelMap
-	//
-	//	res, err := client.FingerprintApi.UpdateEvent(ctx, sdk.EventsUpdateRequest{
-	//		Tag: &tag,
-	//	}, "123")
-	//
-	//	assert.Nil(t, err)
-	//	assert.NotNil(t, res)
-	//	assert.Equal(t, res.StatusCode, 200)
-	//})
-	//
-	//t.Run("Update with empty tag struct", func(t *testing.T) {
-	//	ts := httptest.NewServer(http.HandlerFunc(func(
-	//		w http.ResponseWriter,
-	//		r *http.Request,
-	//	) {
-	//		body, err := io.ReadAll(r.Body)
-	//		assert.NoError(t, err)
-	//
-	//		assert.Equal(t, "{}", string(body))
-	//
-	//		configFile := config.ReadConfig("../config.json")
-	//		integrationInfo := r.URL.Query().Get("ii")
-	//		assert.Equal(t, integrationInfo, fmt.Sprintf("fingerprint-pro-server-go-sdk/%s", configFile.PackageVersion))
-	//		assert.Equal(t, r.URL.Path, "/events/123")
-	//		assert.Equal(t, r.Method, http.MethodPut)
-	//
-	//		apiKey := r.Header.Get("Auth-Api-Key")
-	//		assert.Equal(t, apiKey, "api_key")
-	//
-	//		w.Header().Set("Content-Type", "application/json")
-	//		w.WriteHeader(200)
-	//	}))
-	//	defer ts.Close()
-	//
-	//	cfg := sdk.NewConfiguration()
-	//	cfg.ChangeBasePath(ts.URL)
-	//
-	//	client := sdk.NewAPIClient(cfg)
-	//
-	//	ctx := context.WithValue(context.Background(), sdk.ContextAPIKey, sdk.APIKey{
-	//		Key: "api_key",
-	//	})
-	//
-	//	tag := sdk.ModelMap{}
-	//
-	//	res, err := client.FingerprintApi.UpdateEvent(ctx, sdk.EventsUpdateRequest{
-	//		Tag: &tag,
-	//	}, "123")
-	//
-	//	assert.Nil(t, err)
-	//	assert.NotNil(t, res)
-	//	assert.Equal(t, res.StatusCode, 200)
-	//})
-	//
-	//t.Run("Update with just suspect=false", func(t *testing.T) {
-	//	ts := httptest.NewServer(http.HandlerFunc(func(
-	//		w http.ResponseWriter,
-	//		r *http.Request,
-	//	) {
-	//		body, err := io.ReadAll(r.Body)
-	//		assert.NoError(t, err)
-	//
-	//		assert.Equal(t, "{\"suspect\":false}", string(body))
-	//
-	//		configFile := config.ReadConfig("../config.json")
-	//		integrationInfo := r.URL.Query().Get("ii")
-	//		assert.Equal(t, integrationInfo, fmt.Sprintf("fingerprint-pro-server-go-sdk/%s", configFile.PackageVersion))
-	//		assert.Equal(t, r.URL.Path, "/events/123")
-	//		assert.Equal(t, r.Method, http.MethodPut)
-	//
-	//		apiKey := r.Header.Get("Auth-Api-Key")
-	//		assert.Equal(t, apiKey, "api_key")
-	//
-	//		w.Header().Set("Content-Type", "application/json")
-	//		w.WriteHeader(200)
-	//	}))
-	//	defer ts.Close()
-	//
-	//	cfg := sdk.NewConfiguration()
-	//	cfg.ChangeBasePath(ts.URL)
-	//
-	//	client := sdk.NewAPIClient(cfg)
-	//
-	//	ctx := context.WithValue(context.Background(), sdk.ContextAPIKey, sdk.APIKey{
-	//		Key: "api_key",
-	//	})
-	//
-	//	suspect := false
-	//
-	//	res, err := client.FingerprintApi.UpdateEvent(ctx, sdk.EventsUpdateRequest{
-	//		Suspect: &suspect,
-	//	}, "123")
-	//
-	//	assert.Nil(t, err)
-	//	assert.NotNil(t, res)
-	//	assert.Equal(t, res.StatusCode, 200)
-	//})
-	//
-	//t.Run("Returns ErrorResponse on 404", func(t *testing.T) {
-	//	mockResponse := GetMockResponse[sdk.ErrorResponse]("../test/mocks/errors/404_visitor_not_found.json")
-	//
-	//	ts := httptest.NewServer(http.HandlerFunc(func(
-	//		w http.ResponseWriter,
-	//		r *http.Request,
-	//	) {
-	//
-	//		configFile := config.ReadConfig("../config.json")
-	//		integrationInfo := r.URL.Query().Get("ii")
-	//		assert.Equal(t, integrationInfo, fmt.Sprintf("fingerprint-pro-server-go-sdk/%s", configFile.PackageVersion))
-	//		assert.Equal(t, r.URL.Path, "/events/123")
-	//		assert.Equal(t, r.Method, http.MethodPut)
-	//
-	//		apiKey := r.Header.Get("Auth-Api-Key")
-	//		assert.Equal(t, apiKey, "api_key")
-	//
-	//		w.Header().Set("Content-Type", "application/json")
-	//		w.WriteHeader(404)
-	//		err := json.NewEncoder(w).Encode(mockResponse)
-	//
-	//		if err != nil {
-	//			panic(err)
-	//		}
-	//	}))
-	//	defer ts.Close()
-	//
-	//	cfg := sdk.NewConfiguration()
-	//	cfg.ChangeBasePath(ts.URL)
-	//
-	//	client := sdk.NewAPIClient(cfg)
-	//
-	//	ctx := context.WithValue(context.Background(), sdk.ContextAPIKey, sdk.APIKey{
-	//		Key: "api_key",
-	//	})
-	//
-	//	res, err := client.FingerprintApi.UpdateEvent(ctx, sdk.EventsUpdateRequest{
-	//		LinkedId: "linked_id",
-	//		Tag:      nil,
-	//		Suspect:  &trueValue,
-	//	}, "123")
-	//
-	//	assert.Error(t, err)
-	//	assert.NotNil(t, res)
-	//	assert.Equal(t, res.StatusCode, 404)
-	//
-	//	errorModel := err.(*sdk.ApiError).Model().(*sdk.ErrorResponse)
-	//	assert.IsType(t, errorModel, &sdk.ErrorResponse{})
-	//	assert.Equal(t, errorModel, &mockResponse)
-	//})
-	//
-	//t.Run("Returns ErrorResponse on 400", func(t *testing.T) {
-	//	mockResponse := GetMockResponse[sdk.ErrorResponse]("../test/mocks/errors/400_request_body_invalid.json")
-	//
-	//	ts := httptest.NewServer(http.HandlerFunc(func(
-	//		w http.ResponseWriter,
-	//		r *http.Request,
-	//	) {
-	//
-	//		configFile := config.ReadConfig("../config.json")
-	//		integrationInfo := r.URL.Query().Get("ii")
-	//		assert.Equal(t, integrationInfo, fmt.Sprintf("fingerprint-pro-server-go-sdk/%s", configFile.PackageVersion))
-	//		assert.Equal(t, r.URL.Path, "/events/123")
-	//		assert.Equal(t, r.Method, http.MethodPut)
-	//
-	//		apiKey := r.Header.Get("Auth-Api-Key")
-	//		assert.Equal(t, apiKey, "api_key")
-	//
-	//		w.Header().Set("Content-Type", "application/json")
-	//		w.WriteHeader(400)
-	//		err := json.NewEncoder(w).Encode(mockResponse)
-	//
-	//		if err != nil {
-	//			panic(err)
-	//		}
-	//	}))
-	//	defer ts.Close()
-	//
-	//	cfg := sdk.NewConfiguration()
-	//	cfg.ChangeBasePath(ts.URL)
-	//
-	//	client := sdk.NewAPIClient(cfg)
-	//
-	//	ctx := context.WithValue(context.Background(), sdk.ContextAPIKey, sdk.APIKey{
-	//		Key: "api_key",
-	//	})
-	//
-	//	res, err := client.FingerprintApi.UpdateEvent(ctx, sdk.EventsUpdateRequest{
-	//		LinkedId: "linked_id",
-	//		Tag:      nil,
-	//		Suspect:  &trueValue,
-	//	}, "123")
-	//
-	//	assert.Error(t, err)
-	//	assert.NotNil(t, res)
-	//	assert.Equal(t, res.StatusCode, 400)
-	//
-	//	errorModel := err.(*sdk.ApiError).Model().(*sdk.ErrorResponse)
-	//	assert.IsType(t, errorModel, &sdk.ErrorResponse{})
-	//	assert.Equal(t, errorModel, &mockResponse)
-	//})
-	//
-	//t.Run("Returns ErrorResponse on 403", func(t *testing.T) {
-	//	mockResponse := GetMockResponse[sdk.ErrorResponse]("../test/mocks/errors/403_token_required.json")
-	//
-	//	ts := httptest.NewServer(http.HandlerFunc(func(
-	//		w http.ResponseWriter,
-	//		r *http.Request,
-	//	) {
-	//
-	//		configFile := config.ReadConfig("../config.json")
-	//		integrationInfo := r.URL.Query().Get("ii")
-	//		assert.Equal(t, integrationInfo, fmt.Sprintf("fingerprint-pro-server-go-sdk/%s", configFile.PackageVersion))
-	//		assert.Equal(t, r.URL.Path, "/events/123")
-	//		assert.Equal(t, r.Method, http.MethodPut)
-	//
-	//		apiKey := r.Header.Get("Auth-Api-Key")
-	//		assert.Equal(t, apiKey, "api_key")
-	//
-	//		w.Header().Set("Content-Type", "application/json")
-	//		w.WriteHeader(403)
-	//		err := json.NewEncoder(w).Encode(mockResponse)
-	//
-	//		if err != nil {
-	//			panic(err)
-	//		}
-	//	}))
-	//	defer ts.Close()
-	//
-	//	cfg := sdk.NewConfiguration()
-	//	cfg.ChangeBasePath(ts.URL)
-	//
-	//	client := sdk.NewAPIClient(cfg)
-	//
-	//	ctx := context.WithValue(context.Background(), sdk.ContextAPIKey, sdk.APIKey{
-	//		Key: "api_key",
-	//	})
-	//
-	//	res, err := client.FingerprintApi.UpdateEvent(ctx, sdk.EventsUpdateRequest{
-	//		LinkedId: "linked_id",
-	//		Tag:      nil,
-	//		Suspect:  &trueValue,
-	//	}, "123")
-	//
-	//	assert.Error(t, err)
-	//	assert.NotNil(t, res)
-	//	assert.Equal(t, res.StatusCode, 403)
-	//
-	//	errorModel := err.(*sdk.ApiError).Model().(*sdk.ErrorResponse)
-	//	assert.IsType(t, errorModel, &sdk.ErrorResponse{})
-	//	assert.Equal(t, errorModel, &mockResponse)
-	//})
-	//
-	//t.Run("Returns ErrorResponse on 409", func(t *testing.T) {
-	//	mockResponse := GetMockResponse[sdk.ErrorResponse]("../test/mocks/errors/409_state_not_ready.json")
-	//
-	//	ts := httptest.NewServer(http.HandlerFunc(func(
-	//		w http.ResponseWriter,
-	//		r *http.Request,
-	//	) {
-	//
-	//		configFile := config.ReadConfig("../config.json")
-	//		integrationInfo := r.URL.Query().Get("ii")
-	//		assert.Equal(t, integrationInfo, fmt.Sprintf("fingerprint-pro-server-go-sdk/%s", configFile.PackageVersion))
-	//		assert.Equal(t, r.URL.Path, "/events/123")
-	//		assert.Equal(t, r.Method, http.MethodPut)
-	//
-	//		apiKey := r.Header.Get("Auth-Api-Key")
-	//		assert.Equal(t, apiKey, "api_key")
-	//
-	//		w.Header().Set("Content-Type", "application/json")
-	//		w.WriteHeader(409)
-	//		err := json.NewEncoder(w).Encode(mockResponse)
-	//
-	//		if err != nil {
-	//			panic(err)
-	//		}
-	//	}))
-	//	defer ts.Close()
-	//
-	//	cfg := sdk.NewConfiguration()
-	//	cfg.ChangeBasePath(ts.URL)
-	//
-	//	client := sdk.NewAPIClient(cfg)
-	//
-	//	ctx := context.WithValue(context.Background(), sdk.ContextAPIKey, sdk.APIKey{
-	//		Key: "api_key",
-	//	})
-	//
-	//	res, err := client.FingerprintApi.UpdateEvent(ctx, sdk.EventsUpdateRequest{
-	//		LinkedId: "linked_id",
-	//		Tag:      nil,
-	//		Suspect:  &trueValue,
-	//	}, "123")
-	//
-	//	assert.Error(t, err)
-	//	assert.NotNil(t, res)
-	//	assert.Equal(t, res.StatusCode, 409)
-	//
-	//	errorModel := err.(*sdk.ApiError).Model().(*sdk.ErrorResponse)
-	//	assert.IsType(t, errorModel, &sdk.ErrorResponse{})
-	//	assert.Equal(t, errorModel, &mockResponse)
-	//})
+	t.Run("Update with empty body", func(t *testing.T) {
+		ts := httptest.NewServer(http.HandlerFunc(func(
+			w http.ResponseWriter,
+			r *http.Request,
+		) {
+			body, err := io.ReadAll(r.Body)
+			assert.NoError(t, err)
+
+			assert.Equal(t, "{}\n", string(body))
+
+			integrationInfo := r.URL.Query().Get("ii")
+			assert.Equal(t, integrationInfo, fmt.Sprintf("fingerprint-pro-server-go-sdk/%s", fingerprint.Version))
+			assert.Equal(t, r.URL.Path, "/events/123")
+			assert.Equal(t, r.Method, http.MethodPatch)
+
+			assertAuthorizationHeader(t, r, "api_key")
+
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(200)
+		}))
+		defer ts.Close()
+
+		client := fingerprint.New(fingerprint.WithAPIKey("api_key"), fingerprint.WithBaseURL(ts.URL))
+
+		res, err := client.UpdateEvent(context.Background(), "123", *fingerprint.NewEventUpdate())
+
+		assert.Nil(t, err)
+		assert.NotNil(t, res)
+		assert.Equal(t, 200, res.StatusCode)
+	})
+
+	t.Run("Update with empty tag", func(t *testing.T) {
+		ts := httptest.NewServer(http.HandlerFunc(func(
+			w http.ResponseWriter,
+			r *http.Request,
+		) {
+			body, err := io.ReadAll(r.Body)
+			assert.NoError(t, err)
+
+			assert.Equal(t, "{}\n", string(body))
+
+			integrationInfo := r.URL.Query().Get("ii")
+			assert.Equal(t, integrationInfo, fmt.Sprintf("fingerprint-pro-server-go-sdk/%s", fingerprint.Version))
+			assert.Equal(t, r.URL.Path, "/events/123")
+			assert.Equal(t, r.Method, http.MethodPatch)
+
+			assertAuthorizationHeader(t, r, "api_key")
+
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(200)
+		}))
+		defer ts.Close()
+
+		client := fingerprint.New(fingerprint.WithAPIKey("api_key"), fingerprint.WithBaseURL(ts.URL))
+
+		res, err := client.UpdateEvent(context.Background(), "123", fingerprint.EventUpdate{
+			Tags: nil,
+		})
+
+		assert.Nil(t, err)
+		assert.NotNil(t, res)
+		assert.Equal(t, 200, res.StatusCode)
+	})
+
+	t.Run("Update with empty tag struct", func(t *testing.T) {
+		ts := httptest.NewServer(http.HandlerFunc(func(
+			w http.ResponseWriter,
+			r *http.Request,
+		) {
+			body, err := io.ReadAll(r.Body)
+			assert.NoError(t, err)
+
+			assert.Equal(t, `{"tags":{}}` + "\n", string(body))
+
+			integrationInfo := r.URL.Query().Get("ii")
+			assert.Equal(t, integrationInfo, fmt.Sprintf("fingerprint-pro-server-go-sdk/%s", fingerprint.Version))
+			assert.Equal(t, r.URL.Path, "/events/123")
+			assert.Equal(t, r.Method, http.MethodPatch)
+
+			assertAuthorizationHeader(t, r, "api_key")
+
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(200)
+		}))
+		defer ts.Close()
+
+		client := fingerprint.New(fingerprint.WithAPIKey("api_key"), fingerprint.WithBaseURL(ts.URL))
+
+		res, err := client.UpdateEvent(context.Background(), "123", fingerprint.EventUpdate{
+			Tags: map[string]interface{}{},
+		})
+
+		assert.Nil(t, err)
+		assert.NotNil(t, res)
+		assert.Equal(t, 200, res.StatusCode)
+	})
+	
+	t.Run("Update with just suspect=false", func(t *testing.T) {
+		ts := httptest.NewServer(http.HandlerFunc(func(
+			w http.ResponseWriter,
+			r *http.Request,
+		) {
+			body, err := io.ReadAll(r.Body)
+			assert.NoError(t, err)
+
+			assert.Equal(t, "{\"suspect\":false}\n", string(body))
+
+			integrationInfo := r.URL.Query().Get("ii")
+			assert.Equal(t, integrationInfo, fmt.Sprintf("fingerprint-pro-server-go-sdk/%s", fingerprint.Version))
+			assert.Equal(t, r.URL.Path, "/events/123")
+			assert.Equal(t, r.Method, http.MethodPatch)
+
+			assertAuthorizationHeader(t, r, "api_key")
+
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(200)
+		}))
+		defer ts.Close()
+
+		client := fingerprint.New(fingerprint.WithAPIKey("api_key"), fingerprint.WithBaseURL(ts.URL))
+
+		suspect := false
+
+		res, err := client.UpdateEvent(context.Background(), "123", fingerprint.EventUpdate{
+			Suspect: &suspect,
+		})
+
+		assert.Nil(t, err)
+		assert.NotNil(t, res)
+		assert.Equal(t, 200, res.StatusCode)
+	})
+
+	t.Run("ErrorResponse", func(t *testing.T) {
+		testCases := []struct {
+			StatusCode       int
+			MockResponsePath string
+		}{
+			{404, "mocks/errors/404_request_not_found.json"},
+			{400, "mocks/errors/400_request_body_invalid.json"},
+			{403, "mocks/errors/403_secret_api_key_required.json"},
+			{409, "mocks/errors/409_state_not_ready.json"},
+		}
+
+		for _, testCase := range testCases {
+			t.Run(fmt.Sprintf("Returns ErrorResponse on %d", testCase.StatusCode), func(t *testing.T) {
+				mockResponse := GetMockResponse[fingerprint.ErrorResponse](testCase.MockResponsePath)
+				ts := httptest.NewServer(http.HandlerFunc(func(
+					w http.ResponseWriter,
+					r *http.Request,
+				) {
+					integrationInfo := r.URL.Query().Get("ii")
+					assert.Equal(t, integrationInfo, fmt.Sprintf("fingerprint-pro-server-go-sdk/%s", fingerprint.Version))
+					assert.Equal(t, r.URL.Path, "/events/123")
+					assert.Equal(t, r.Method, http.MethodPatch)
+
+					assertAuthorizationHeader(t, r, "api_key")
+
+					w.Header().Set("Content-Type", "application/json")
+					w.WriteHeader(testCase.StatusCode)
+					err := json.NewEncoder(w).Encode(mockResponse)
+					if err != nil {
+						panic(err)
+					}
+				}))
+				defer ts.Close()
+
+				client := fingerprint.New(fingerprint.WithAPIKey("api_key"), fingerprint.WithBaseURL(ts.URL))
+
+				res, err := client.UpdateEvent(context.Background(), "123", fingerprint.EventUpdate{
+					LinkedId: &linkedIdValue,
+					Tags:     nil,
+					Suspect:  &trueValue,
+				})
+
+				assertErrorResponse(t, testCase.StatusCode, mockResponse, res, err)
+			})
+		}
+	})
 }
