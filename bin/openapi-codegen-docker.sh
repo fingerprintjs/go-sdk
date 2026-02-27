a1d5e41384e79c7bbf5d08eba879dc0bf5df0172
Przemysław Żydek <przemyslawzydek@gmail.com>
2026-02-27 14:49:40 +0100

    build: add cleanup script

--- bin/openapi-codegen-docker.sh
+++ bin/openapi-codegen-docker.sh
@@ -4,6 +4,33 @@ set -e
 
 PACKAGE_VERSION=$(node -p "require('./package.json').version")
 
+cleanup() {
+  LIST_FILE=".openapi-generator/FILES"
+  
+  if [[ ! -f "$LIST_FILE" ]]; then
+      echo "Error: File '$LIST_FILE' not found."
+      exit 1
+  fi
+  
+  while IFS= read -r filepath || [[ -n "$filepath" ]]; do
+      # Skip empty lines
+      [[ -z "$filepath" ]] && continue
+  
+      # Check if the target file exists before removing
+      if [[ -e "$filepath" ]]; then
+          rm "$filepath"
+          if [[ $? -eq 0 ]]; then
+              echo "Successfully removed: $filepath"
+          else
+              echo "Failed to remove: $filepath"
+          fi
+      else
+          echo "Skip: '$filepath' does not exist."
+      fi
+      
+    done < "$LIST_FILE"
+}
+
 run_generator() {
   docker run --rm -u "$(id -u):$(id -g)" -v "${PWD}:/local" openapitools/openapi-generator-cli:v7.19.0 generate \
       -c /local/openapi-config.yml \
@@ -14,5 +41,6 @@ run_generator() {
       # Please use the config file for all other configuration
 }
 
+cleanup
 run_generator
 gofmt -w .
