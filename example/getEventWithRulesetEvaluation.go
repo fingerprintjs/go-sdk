ef70f9e1933aeccd6141a5046f673c8648bfaacd
necipallef <necipallef@gmail.com>
2026-03-05 09:34:12 +0300

    chore: fix ruleset evaluation example

--- example/getEventWithRulesetEvaluation.go
+++ example/getEventWithRulesetEvaluation.go
@@ -32,7 +32,13 @@ func main() {
 
 	if response.RuleAction != nil {
 		if response.RuleAction.EventRuleActionAllow != nil {
-			fmt.Printf("action is allow, rule id: %s, rule expression: %s\n", *response.RuleAction.EventRuleActionAllow.RuleID, *response.RuleAction.EventRuleActionAllow.RuleExpression)
+			fmt.Println("action is allow")
+			if response.RuleAction.EventRuleActionAllow.RuleID != nil {
+				fmt.Printf("rule id: %s\n", *response.RuleAction.EventRuleActionAllow.RuleID)
+			}
+			if response.RuleAction.EventRuleActionAllow.RuleExpression != nil {
+				fmt.Printf("rule expression: %s\n", *response.RuleAction.EventRuleActionAllow.RuleExpression)
+			}
 			if response.RuleAction.EventRuleActionAllow.RequestHeaderModifications != nil {
 				fmt.Printf("request header modifications to set %v, to append %v, to remove %v\n",
 					response.RuleAction.EventRuleActionAllow.RequestHeaderModifications.Set,
@@ -40,12 +46,17 @@ func main() {
 					response.RuleAction.EventRuleActionAllow.RequestHeaderModifications.Remove)
 			}
 		} else if response.RuleAction.EventRuleActionBlock != nil {
-			fmt.Printf("action is block. rule id: %s, rule expression: %s, body %s statusCode: %d headers: %v\n",
-				*response.RuleAction.EventRuleActionBlock.RuleID,
-				*response.RuleAction.EventRuleActionBlock.RuleExpression,
+			fmt.Printf("action is block. body %s statusCode: %d headers: %v\n",
 				*response.RuleAction.EventRuleActionBlock.Body,
 				*response.RuleAction.EventRuleActionBlock.StatusCode,
 				response.RuleAction.EventRuleActionBlock.Headers)
+
+			if response.RuleAction.EventRuleActionBlock.RuleID != nil {
+				fmt.Printf("rule id: %s\n", *response.RuleAction.EventRuleActionBlock.RuleID)
+			}
+			if response.RuleAction.EventRuleActionBlock.RuleExpression != nil {
+				fmt.Printf("rule expression: %s\n", *response.RuleAction.EventRuleActionBlock.RuleExpression)
+			}
 		} else {
 			fmt.Println("action is unexpected (please make sure library is at the latest version)")
 		}
